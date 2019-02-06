7858432e70103c62d583ddc0f18322ded3488702
peromage <peromage@outlook.com>
2019-02-06 12:32:58 -0500

    Organized file structure

---
+++ DV-ReportAnalytics/Events/UserMessageEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DV_ReportAnalytics.UI
+{
+        public class UserMessageEventArgs : EventArgs
+        {
+            public string Message { get; }
+
+            public UserMessageEventArgs(string message)
+            {
+                this.Message = message;
+            }
+
+        }
+        public delegate void UserMessageEventHandler(object sender, UserMessageEventArgs e);
+        
+}
