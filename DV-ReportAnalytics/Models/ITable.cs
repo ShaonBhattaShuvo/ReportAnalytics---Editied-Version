13ef37a615ecb745acc5f1155c7f5c114eaf859e
peromage <peromage@outlook.com>
2019-02-11 15:12:27 -0500

    Added type definition and table interface

---
+++ DV-ReportAnalytics/Models/ITable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DV_ReportAnalytics.Types.Table;
+
+namespace DV_ReportAnalytics.Models
+{
+    // KR: key row, KC: key columnm, V: value
+    interface ITable<KR, KC, V>
+    {
+        void SetValue(KR row, KC column, V value);
+        void SetTable();
+        void Transpose();
+        string GetName();
+        V GetValue(KR row, KC column);
+        Table GetValueByRows(KR[] rows);
+        Table GetValueByColumns(KC[] columns);
+        Table GetTable();
+        Dimension GetDimension();
+    }
+}
---
+++ DV-ReportAnalytics/Types/TTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DV_ReportAnalytics.Types.Table
+{
+    class Table<KR, KC, V>: Dictionary<KR, Dictionary<KC, V>>{}
+
+    struct Dimension
+    {
+        int rows;
+        int columns;
+    }
+
+    struct Headers<KR, KC>
+    {
+        KR row;
+        KC column;
+    }
+}
