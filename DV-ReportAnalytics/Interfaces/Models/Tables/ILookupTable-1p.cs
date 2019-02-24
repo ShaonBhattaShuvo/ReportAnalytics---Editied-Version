27d53895043cfd5134ba394cdeef092758a400b0
peromage <peromage@outlook.com>
2019-02-23 20:32:42 -0500

    Added 1p lookup table interface

---
+++ DV-ReportAnalytics/Interfaces/Models/Tables/ILookupTable-1p.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DV_ReportAnalytics.Types;
+
+namespace DV_ReportAnalytics.Models
+{
+    interface ILookupTable<Tkey, TValue>
+    {
+        string Name { get; }
+        string keyName { get; }
+        string valueName { get; }
+
+        // use indexer to get value in a convenient way
+        TValue this[Tkey key] { set; get; }
+
+        // get table
+        TData2D<Tkey, TValue> GetData2D(Tkey[] range);
+
+        // get the number of elements
+        int GetDimension();
+    }
+}
