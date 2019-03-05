29616efe864e21e092da5e386bb7b9aa11205c7d
peromage <peromage@outlook.com>
2019-03-05 16:29:25 -0500

    Added workbook model events

--- DV-ReportAnalytics/Events/WorkbookModelEvents.cs
+++ DV-ReportAnalytics/Events/WorkbookModelEvents.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
 
 namespace DV_ReportAnalytics.Events
 {
     class WorkbookModelViewSubmitEvent : EventArgs
     {
+        public XmlDocument Config { get; }
+        public WorkbookModelViewSubmitEvent(XmlDocument config)
+        {
+            Config = config;
+        }
     }
 
     delegate void WorkbookModelViewSubmitEventHandler(object sender, WorkbookModelViewSubmitEvent e);
 
     class WorkbookModelUpdateEvent : EventArgs
     {
-
+        // the whole excel file in binary
+        public byte[] Buffer { get; }
+        public WorkbookModelUpdateEvent(byte[] buffer)
+        {
+            Buffer = buffer;
+        }
     }
 
     delegate void WorkbookModelUpdateEventHandler(object sender, WorkbookModelUpdateEvent e);
