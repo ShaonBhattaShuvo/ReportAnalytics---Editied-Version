03afea6bca58f870fb91be19fb141a480bfacd94
[ShaonBhatta Shuvo] <shuvocste21@gmail.com>
2020-06-01 13:50:47 -0400

    CLI as single argument added

--- DV_ReportAnalytics.App/Presenters/EPTPresenterProxy.cs
+++ DV_ReportAnalytics.App/Presenters/EPTPresenterProxy.cs
@@ -1,4 +1,10 @@
-﻿namespace DV_ReportAnalytics.App.Presenters
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DV_ReportAnalytics.App.Presenters
 {
     public class EPTPresenterProxy
     {
@@ -15,6 +21,35 @@
             presenter.DrawTablesCLI(filePath);
             //presenter.Export(filePath);
         }
+        public void Screenshot(string url)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");//Comment if we want to see the window. 
+            var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options);
+            driver.Manage().Window.Size = new System.Drawing.Size(800, 7100);
+            driver.Navigate().GoToUrl(url);
+            var screenshot = (driver as ITakesScreenshot).GetScreenshot();
+            Char charRange = '\\';
+            int startIndex = url.IndexOf(charRange);
+            int endIndex = url.LastIndexOf(charRange);
+            int length = endIndex - startIndex + 1;
+            string outputPath = url.Substring(startIndex, length) + "output.png";
+            screenshot.SaveAsFile(outputPath);
+            driver.Close();
+            driver.Quit();
+        }
+        public void WriteSurfaceHtml(string html, string destinationFileLocation)
+        {
+            Char charRange = '\\';
+            int startIndex = destinationFileLocation.IndexOf(charRange);
+            int endIndex = destinationFileLocation.LastIndexOf(charRange);
+            int length = endIndex - startIndex + 1;
+            string outputPath = destinationFileLocation.Substring(startIndex, length) + "output.html";
+            File.WriteAllText(outputPath, html);
+        }
+        public void OpenHTML(string url)
+        {
+            System.Diagnostics.Process.Start(url);
+        }
     }
 }
- 
--- DV_ReportAnalytics/Program.cs
+++ DV_ReportAnalytics/Program.cs
@@ -6,9 +6,6 @@ using System.Windows.Forms;
 using DV_ReportAnalytics.App;
 using DV_ReportAnalytics.App.Presenters;
 using DV_ReportAnalytics.GUI;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-
 namespace DV_ReportAnalytics
 {
     static class Program
@@ -28,16 +25,29 @@ namespace DV_ReportAnalytics
             // must be before any calls to Console.WriteLine()
             AttachConsole(ATTACH_PARENT_PROCESS);
 
-            if (args.Length == 2)
+            EPTPresenterProxy proxy = new EPTPresenterProxy();
+            if (args.Length == 1) 
+            {
+                // sending the enter key is not really needed, but otherwise the user thinks the app is still running by looking at the commandline. The enter key takes care of displaying the prompt again.
+                //SpreadsheetGearWorkbookViewController svc = new SpreadsheetGearWorkbookViewController();
+                proxy.WriteSurfaceHtml(proxy.GetSurfaceHTML(args[0]), args[0]);
+                //Opening the html file in default browser
+                //proxy.OpenHTML(args[0]);
+                //Capturing Screenshop as png format. 
+                proxy.Screenshot(args[0]);
+                System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+                Application.Exit();
+                // pass input/output path as arguent e.g. "C:\Users\Downloads\test-Copy.xlsx"
+            }
+            else if (args.Length == 2)
             {   
                 // sending the enter key is not really needed, but otherwise the user thinks the app is still running by looking at the commandline. The enter key takes care of displaying the prompt again.
-                EPTPresenterProxy proxy = new EPTPresenterProxy();
                 //SpreadsheetGearWorkbookViewController svc = new SpreadsheetGearWorkbookViewController();
-                WriteSurfaceHtml(proxy.GetSurfaceHTML(args[0]), args[1]);
+                proxy.WriteSurfaceHtml(proxy.GetSurfaceHTML(args[0]), args[1]);
                 //Opening the html file in default browser
-                OpenHTML(args[1]);
+                proxy.OpenHTML(args[1]);
                 //Capturing Screenshop as png format. 
-                Screenshot(args[1]);
+                proxy.Screenshot(args[1]);
                 System.Windows.Forms.SendKeys.SendWait("{ENTER}");
                 Application.Exit();
                 // pass input/output path as arguent e.g. "C:\Users\Downloads\test-Copy.xlsx" "C:\Users\Downloads\test-Copy.Result.html"
@@ -57,30 +67,6 @@ namespace DV_ReportAnalytics
                 Application.Run((Form)presenter.View);
             }
 
-        }
-        
-        private static void WriteSurfaceHtml(string html, string destinationFileName)
-        {
-            File.WriteAllText(destinationFileName, html);
-        }
-        private static void OpenHTML(string url) {
-            System.Diagnostics.Process.Start(url);
-        }
-        private static void Screenshot(string url) {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("headless");//Comment if we want to see the window. 
-            var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options);
-            driver.Manage().Window.Size = new System.Drawing.Size(800, 7100);
-            driver.Navigate().GoToUrl(url);
-            var screenshot = (driver as ITakesScreenshot).GetScreenshot();
-            Char charRange = '\\';
-            int startIndex = url.IndexOf(charRange);
-            int endIndex = url.LastIndexOf(charRange);
-            int length = endIndex - startIndex + 1;
-            string outputPath = url.Substring(startIndex, length) + "screenshot.png";
-            screenshot.SaveAsFile(outputPath);
-            driver.Close();
-            driver.Quit();
         }
     }
 }
