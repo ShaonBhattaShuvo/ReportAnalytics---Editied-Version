d41d7f8b065f80b5cadca0a9dbc72a244766bc1c
[ShaonBhatta Shuvo] <shuvocste21@gmail.com>
2020-06-21 20:08:59 -0400

    Added image in excel GUI mode

--- DV_ReportAnalytics.App/SpreadsheetGear/SpreadSheetGearTableExtensions.cs
+++ DV_ReportAnalytics.App/SpreadsheetGear/SpreadSheetGearTableExtensions.cs
@@ -77,7 +77,49 @@ namespace DV_ReportAnalytics.App.SpreadsheetGear
                     current = range.All.CellRight().CellRight();
                 }
             }
-                        return tableRanges.AsEnumerable();
+            //AddImage to the sheet
+            string[] plot_names = new string[] {"Copper_Loss", "Output_Power", "Input_Power", "Rotational_Loss", "Total_Loss","DC_Power",
+                "Calculated_System_Efficiency", "Calculated_Motor_Efficiency", "Calculated_Inverter_Efficiency", "Inverter_Loss", "Motor_Loss",
+                "System_Loss", "CurrentArms", "CurrentArmsAvr"};
+            for (int i = 0,t=825; i < plot_names.Length; i++)
+            {
+                System.String imageFile = @"C:\Temp\DV_Imagefiles\" + plot_names[i]+".png";
+                // Get the width and height of the picture in pixels and convert to 
+                // points for use in the call to AddPicture.  This step is only 
+                // necessary if the actual picture size is to be used and that size 
+                // is unknown.  Another option is to calculate the width and height  
+                // in row and column coordinates in the same manner as left and top below. 
+                double width;
+                double height;
+                System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+                using (image)
+                {
+                    width = image.Width * 72.0 / image.HorizontalResolution;
+                    height = image.Height * 72.0 / image.VerticalResolution;
+                }
+
+                // Calculate the left and top placement of the picture by converting  
+                // row and column coordinates to points.  Use fractional values to  
+                // get coordinates anywhere in between row and column boundaries. 
+                //SpreadsheetGear.IWorksheetWindowInfo windowInfo = worksheet.WindowInfo;
+                // Convert from the center of column B to points. 
+                //double left = windowInfo.ColumnToPoints(1.5);
+                // Convert from the center of row 1 to points. 
+                //double top = windowInfo.RowToPoints(1.5);
+
+                // Add the picture from file. 
+                //worksheet.Shapes.AddPicture(imageFile, left, top, width, height);
+                if (i % 2 == 0)
+                {
+                    worksheet.Shapes.AddPicture(imageFile, 1.5, t, 280, 250);
+                }
+                else
+                {
+                    worksheet.Shapes.AddPicture(imageFile, 281.5, t, 280, 250);
+                    t = t + 255;
+                }
+            }
+            return tableRanges.AsEnumerable();
         }
         public static void ApplyHeatMap(this TableDataRange source, Color low, Color mid, Color high)
         {
